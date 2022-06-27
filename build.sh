676abe973d045ad0fbbf9410da20c78e9b86e3f2
st3p99 <pernastefano99@gmail.com>
2022-06-27 20:50:09 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,22 +6,24 @@ then git pull && cd ..;
 else git clone https://github.com/flutter/flutter.git; fi
 
 FLUTTER=flutter/bin/flutter
+export PATH="$PATH":"$FLUTTER"
 
 # Configure flutter
 FLUTTER_CHANNEL=stable
-$FLUTTER channel $FLUTTER_CHANNEL
-$FLUTTER config --enable-web
+flutter channel $FLUTTER_CHANNEL
+flutter config --enable-web
 
 # Setup dart
 DART=`echo $FLUTTER | sed 's/flutter$/cache\/dart-sdk\/bin\/dart/'`
-echo $DART
+export PATH="$PATH":"$DART"
 
 echo "installing flutter_cors"
-$DART pub global activate flutter_cors
+dart pub global activate flutter_cors
 export PATH="$PATH":"$HOME/.pub-cache/bin"
+echo "disabling flutter cors"
 fluttercors -db -p $FLUTTER
 
 # Build flutter for web
 
-$FLUTTER build web --release
+flutter build web --release
 echo "OK"
